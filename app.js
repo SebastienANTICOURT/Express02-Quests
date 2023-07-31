7e79dcc31099c5b07ed1bece59f26b430227c9ab
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-07-31 21:26:38 +0200

    express04

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 const  userHandlers = require ("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
